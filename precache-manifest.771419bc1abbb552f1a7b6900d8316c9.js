55a8617290ebc42a7261079b0392e37acdfe387b
ewill025 <ericwilliamsjr@live.com>
2020-07-26 12:45:11 -0400

    Updates

---
+++ precache-manifest.771419bc1abbb552f1a7b6900d8316c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fbc55d7355eb3591adb9d1cfdd7f6b",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "db7d2c6b156e79c68470",
+    "url": "/monsters-app/static/css/main.11d08ab3.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7d2c6b156e79c68470",
+    "url": "/monsters-app/static/js/main.86b80b50.chunk.js"
+  },
+  {
+    "revision": "9cd392af60b73ea37e8f",
+    "url": "/monsters-app/static/js/runtime-main.ce4bea50.js"
+  }
+]);
